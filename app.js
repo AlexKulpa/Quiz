beb5f9293403cafe1402abd662112a97754cef15
AlexKulpa <82471798+AlexKulpa@users.noreply.github.com>
2021-10-22 19:52:24 +0200

    working_quiz

---
+++ app.js
@@ -0,0 +1,19 @@
+const correctAnswers=['B','B','B'];
+const form = document.querySelector('.quiz-form');
+const result = document.querySelector('.result');
+
+form.addEventListener('submit',e=>{
+    e.preventDefault();
+
+    let score = 0;
+    const userAnswers = [form.q1.value,form.q2.value,form.q3.value];
+
+    userAnswers.forEach((answer,index) => {
+        if(answer === correctAnswers[index]){
+            score += (1/3)*100;
+        }
+    });
+
+    result.querySelector('span').textContent = `${score}%`;
+    result.classList.remove('d-none');
+});
